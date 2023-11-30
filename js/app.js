dd41a589a10f1fc9613ad80be9c13f37ae7454f4
Sigachevsky <151017803+Sigachevsky@users.noreply.github.com>
2023-11-30 22:49:00 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+new Swiper('.slider', {
+/* 	direction: 'vertical', */
+	speed: 2200,
+	mousewheel: {
+		enabled: true,
+		sensitivity: 2.9
+	},
+	spaceBetween: 18,
+	parallax: true,
+	freeMode: true,
+})
+
